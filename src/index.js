2cbe5ae993c568181ded4e7dece61cf4dc4b0430
Anish Sharma <sharmaanish2277@gmail.com>
2026-04-02 01:25:17 +0530

    added lock file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Routes
 app.get('/', (req, res) => {
   res.send('🚀 Node App is running successfully!');
 });
@@ -11,6 +12,10 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
 
-app.listen(PORT, () => {
+app.get('/version', (req, res) => {
+  res.json({ version: "v2", message: "Updated deployment 🚀" });
+});
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
